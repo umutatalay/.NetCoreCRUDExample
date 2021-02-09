eadc3dd268527d6f3abc68be0fd3067382bef881
umutatalay <umutatalayumut@gmail.com>
2021-02-10 01:43:44 +0300

    commit

---
+++ NetCoresubat/Repositories/DapperCategoriesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using NetCoresubat.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoresubat.Repositories
+{
+    public class DapperCategoriesRepository
+    {
+        public List<category> GetAllProducts()
+        {
+            using var connection = new SqlConnection("server=DESKTOP-IT4752E; database=DbCoreProductsCategories; Integrated Security=True;");
+
+            return connection.GetAll<category>().ToList();
+        }
+    }
+}
---
+++ NetCoresubat/Repositories/DapperProductsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using NetCoresubat.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoresubat.Repositories
+{
+    public class DapperProductsRepository
+    {
+        public List<products> GetAllProducts()
+        {
+            using var connection = new SqlConnection("server=DESKTOP-IT4752E; database=DbCoreProductsCategories; Integrated Security=True;");
+
+            return connection.GetAll<products>().ToList();
+        }
+    }
+}
